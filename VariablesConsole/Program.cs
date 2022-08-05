45c51cbde695afb80fe07a42c70e5a36f6e4f488
ihababoudiab@gmail.com <ihababoudiab@gmail.com>
2022-08-04 17:08:27 -0700

    initial commit

---
+++ VariablesConsole/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariablesConsole
+{
+    internal class Program
+    {
+        public static bool True { get; private set; }
+
+        static void Main(string[] args)
+        {
+            string myName = "Ihab";
+            int myProject = 2;
+            double myPoints = 9.98d;
+         
+
+            Console.WriteLine($" Hello, My name is {myName},");
+            Console.WriteLine( $" Here is my project number {myProject}, my average points are {myPoints}.");
+
+          }
+    }
+}
